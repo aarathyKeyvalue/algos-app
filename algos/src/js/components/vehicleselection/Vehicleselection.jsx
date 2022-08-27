da5163ce6302c4244ab0e16a6519fb5fd5c3a797
saranya-vijayan <saranya@keyvalue.systems>
2022-08-27 18:38:30 +0530

    buttonmodified

---
+++ algos/src/js/components/vehicleselection/Vehicleselection.jsx
@@ -0,0 +1,50 @@
+
+import lcv from "../../../assets/main/LCV.svg";
+import trailer from "../../../assets/main/trailer.svg";
+import truck from "../../../assets/main/Truck.svg";
+import container from "../../../assets/main/container.svg";
+import "./styles.css";
+const Vehicleselection=()=>{
+return(
+<div>
+    <div className="backgroundclass">
+    <div className='hyva'>
+    </div>
+    <div className="namediv">
+    Hyva
+    </div>
+    </div>
+
+    <div className="backgroundclass">
+    <div className='container'>
+    </div>
+    <div className="namediv">
+    Hyva
+    </div>
+    </div>
+    <div className="backgroundclass">
+    <div className='lcv'>
+    </div>
+    <div className="namediv">
+    Hyva
+    </div>
+    </div>
+    <div className="backgroundclass">
+    <div className='trailer'>
+    </div>
+    <div className="namediv">
+    Hyva
+    </div>
+    </div>
+    <div className="backgroundclass">
+    <div className='truck'>
+    </div>
+    <div className="namediv">
+    Hyva
+    </div>
+    </div>
+
+</div>
+)
+};
+export default Vehicleselection;
